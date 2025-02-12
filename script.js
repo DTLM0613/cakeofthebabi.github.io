aee4bb5fe7f1f4b39df7b2ef9a55552c0887babd
DTLM0613 <43031290+DTLM0613@users.noreply.github.com>
2025-02-13 01:01:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
+    "Sure babi?",
+    "Sure na lagi?",
+    "Ayaw hin parayaw..",
+    "Na sobra kana...",
+    "HOY YES PLEASE GAD",
+    "Babi.... please be my valentine?",
     "I will be very sad...",
     "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Sige dire naak mapakaian....",
+    "JOKE LAAAAAAAA, say yes please! ❤️"
 ];
 
 let messageIndex = 0;
